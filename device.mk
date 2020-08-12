b72cac294d5390a52f4e52c16a26e152d39ef29d
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    spes: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -399,6 +399,10 @@ PRODUCT_PACKAGES += \
     SpesTelephonyOverlay \
     SpesWifiOverlay
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
