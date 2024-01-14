e79c20bc29a9b9e500d0057709addd3a98b07872
Cyber Knight <cyberknight755@gmail.com>
2024-01-14 23:17:53 +0000

    spes: Support doze double/single tap sensor
    * AOSPA/android_device_xiaomi_sunny@97dcb58
    * AOSPA/android_device_xiaomi_sunny@d6c5d56
    * AOSPA/android_device_xiaomi_sunny@2bfc290
    * AOSPA/android_device_xiaomi_sunny@98fca00
    * AOSPA/android_device_xiaomi_sunny@e06fa26
    
    Change-Id: Ic6b26b408d0a6cb809085287b5ace1894cc0f691
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -468,7 +468,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.xiaomi
 
 # Service Tracker
 PRODUCT_PACKAGES += \
