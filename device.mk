d5d556be1c0b6060eae1dc7d117c785e49448f4b
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-01-14 23:10:25 +0000

    spes: Move hals.conf to device tree
    Change-Id: Ic783df474a85784ae971d2f7e22adfed63c80a5c
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -474,6 +474,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.2.vendor
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
