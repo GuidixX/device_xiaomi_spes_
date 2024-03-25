f0ef693cb176422355a2d6671ed2735834a6c306
Dhina17 <dhinalogu@gmail.com>
2024-03-25 23:52:21 +0530

    spes: Build QTI perf 2.2 lib
    Change-Id: I72cf4b1947886dc8a3288e96a9bd024badc05d18
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- device.mk
+++ device.mk
@@ -421,6 +421,9 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
