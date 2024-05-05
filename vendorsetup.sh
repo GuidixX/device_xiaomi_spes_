05f6e6f8372140ea9505977f23b7b4e9935ae3fe
Guidix <guidixm@gmail.com>
2024-05-05 23:00:44 +0300

    spes: Update ViperFX Apk if checksum is different

---
+++ vendorsetup.sh
@@ -0,0 +1,33 @@
+# ROM source patches
+
+color="\033[0;32m"
+end="\033[0m"
+
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+echo -e "${color}ViperFX Patch{end}"
+sleep 1
+
+# Clone Viper Repo
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+# Update Viper4AndroidFX APK
+VIPER_DIR="packages/apps/ViPER4AndroidFX"
+APK_DIR="system/app/ViPER4AndroidFX"
+URL="https://sourceforge.net/projects/guidixproject/files/files/ViPER4AndroidFX.apk"
+
+# Calculate the checksum of Viper.apk
+if [ -d "$VIPER_DIR" ]; then
+current_checksum=$(md5sum "${VIPER_DIR}/${APK_DIR}/ViPER4AndroidFX.apk" | awk '{ print $1 }')
+
+# Expected Viper checksum
+expected_checksum="17bac8664245084f9696d979f9c4a9a8"
+
+# Compare the current checksum with the expected checksum
+ if [ "$current_checksum" != "$expected_checksum" ]; then
+   echo -e "${color}Patching Viper Please Wait ${end}"
+   rm -rf ${VIPER_DIR}/${APK_DIR}/ViPER4AndroidFX.apk
+   wget ${URL} -O ${VIPER_DIR}/${APK_DIR}/ViPER4AndroidFX.apk
+ fi
+fi
