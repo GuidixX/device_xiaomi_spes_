45970a5e2486c7a34bd418602bc4fd4f377d6b44
Guidix <guidixm@gmail.com>
2024-05-08 21:21:19 +0300

    spes: Add back lineage health service

--- device.mk
+++ device.mk
@@ -331,6 +331,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-service
