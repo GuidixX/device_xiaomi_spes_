5adc3a4c4d2557ad6ab8f8ed3b232f14b600f514
Guidix <guidixm@gmail.com>
2024-07-11 01:59:06 +0300

    spes: Drop XiaomiParts
    Moving to displayFeatures and separated Refreshrate Implementation

--- device.mk
+++ device.mk
@@ -415,6 +415,13 @@ PRODUCT_PACKAGES += \
 # Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
 
+# Refreshrate
+PRODUCT_PACKAGES += \
+    RefreshRateParts
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/refreshrate/privapp-permissions-refresh-rate-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-refresh-rate-parts.xml
+
 # RIL
 PRODUCT_PACKAGES += \
     libcurl.vendor \
@@ -535,8 +542,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-# XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-    
