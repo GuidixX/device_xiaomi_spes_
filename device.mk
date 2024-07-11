f5b1c573c0e17df0b5364a9a1c605ae6823e3417
Guidix <guidixm@gmail.com>
2024-07-11 04:31:32 +0300

    spes: Build Display Features

--- device.mk
+++ device.mk
@@ -215,6 +215,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# DisplayFeatures
+PRODUCT_PACKAGES += \
+    DisplayFeatures \
+    DisplayFeaturesOverlay
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
