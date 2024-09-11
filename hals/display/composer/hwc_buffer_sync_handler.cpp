5b4d9b141b018686aee1f635d5d18f566c87b559
Guidix <81136142+GuidixX@users.noreply.github.com>
2024-09-12 08:15:44 +1200

    spes: hals: display: Fix undeclared identifier 'assert'
    hwc_buffer_sync_handler.cpp:112:3: error: use of undeclared identifier 'assert'
      112 |   assert(false);
          |   ^
    device/xiaomi/spes/hals/display/composer/hwc_buffer_sync_handler.cpp:118:3: error: use of undeclared identifier 'assert'
      118 |   assert(false);
          |   ^

--- hals/display/composer/hwc_buffer_sync_handler.cpp
+++ hals/display/composer/hwc_buffer_sync_handler.cpp
@@ -32,6 +32,7 @@
 #include <utils/constants.h>
 #include <utils/debug.h>
 #include <utils/fence.h>
+#include <cassert> // For assert
 
 #include "hwc_debugger.h"
 #include "hwc_buffer_sync_handler.h"
