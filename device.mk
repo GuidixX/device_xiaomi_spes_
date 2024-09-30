73f9d4ce01a8f9caaa0bf6c73bf6bf65303904d9
Guidix <guidixm@gmail.com>
2024-09-30 08:32:05 +0300

    spes: Configure Aditional Framerates for Derp Flash Camera

--- device.mk
+++ device.mk
@@ -380,7 +380,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
-    SpesApertureOverlay \
+    SpesFlashOverlay \
     SpesCarrierConfigOverlay \
     SpesDialerOverlay \
     SpesFrameworksOverlay \
